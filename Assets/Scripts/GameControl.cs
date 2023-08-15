419a0d5217c3cacff2c701dd9f378133bcec85b2
hagi845 <gya132811@gmail.com>
2023-08-15 21:34:18 +0900

    左右キー押下時にボールが判定ブロックと重なってなければゲームオーバー

--- Assets/Scripts/GameControl.cs
+++ Assets/Scripts/GameControl.cs
@@ -11,8 +11,9 @@ public class GameControl : MonoBehaviour
 
     public GameObject GameOverDialog;
 
+    public GameObject ball;
+
     private float positionY = -3.9f;
-    private float positionZ = 2.0f;
 
     private GameObject currentBlock;
     private KeyCode lastKeyPressed = KeyCode.None;
@@ -27,31 +28,51 @@ public class GameControl : MonoBehaviour
 
     private void Update()
     {
+        if (GameOverDialog.activeSelf) return;
         if (currentBlock == null) return;
 
         if (Input.GetKeyDown(KeyCode.LeftArrow) && lastKeyPressed != KeyCode.LeftArrow)
         {
+            if (!CheckOverlap())
+            {
+                GameOver();
+                return; 
+            }
             currentBlock.transform.position = CreateRandomVectorLeft();
             lastKeyPressed = KeyCode.LeftArrow;
         }
         else if (Input.GetKeyDown(KeyCode.RightArrow) && lastKeyPressed != KeyCode.RightArrow)
         {
+            if (!CheckOverlap())
+            {
+                GameOver();
+                return;
+            }
             currentBlock.transform.position = CreateRandomVectorRight();
             lastKeyPressed = KeyCode.RightArrow;
         }
     }
 
-    private Vector3 CreateRandomVectorLeft() => CreateRandomVector(-6.5f, -1.5f);
-    private Vector3 CreateRandomVectorRight() => CreateRandomVector(1.5f, 6.5f);
+    private Vector2 CreateRandomVectorLeft() => CreateRandomVector(-6.5f, -1.5f);
+    private Vector2 CreateRandomVectorRight() => CreateRandomVector(1.5f, 6.5f);
 
-    private Vector3 CreateRandomVector(float minX, float maxX)
+    private Vector2 CreateRandomVector(float minX, float maxX)
     {
         float randomX = Random.Range(minX, maxX);
-        return new Vector3(randomX, positionY, positionZ);
+        return new Vector2(randomX, positionY);
     }
 
     public void GameOver()
     {
         GameOverDialog.SetActive(true);
     }
+
+    private bool CheckOverlap()
+    {
+        Bounds ballBounds = ball.GetComponent<Renderer>().bounds;
+        Bounds barBounds = currentBlock.GetComponent<Renderer>().bounds;
+
+        return ballBounds.Intersects(barBounds);
+    }
+
 }
