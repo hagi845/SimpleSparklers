e9f8721a18c00f335a9d2cbc7f3297a0752ab9d2
hagi845 <gya132811@gmail.com>
2023-08-15 15:36:27 +0900

    左右キーで任意の場所にブロッックが表示されるようにする

---
+++ Assets/Scripts/GameControl.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControl : MonoBehaviour
+{
+    /// <summary>
+    /// ブロックのプレハブ
+    /// </summary>
+    public GameObject blockObjectPrefab;
+
+    private float positionY = -3.9f;
+    private float positionZ = 2.0f;
+
+    private GameObject currentBlock;
+    private KeyCode lastKeyPressed = KeyCode.None; 
+
+    private void Start()
+    {
+        currentBlock = Instantiate(blockObjectPrefab, CreateRandomVectorRight(), Quaternion.identity);
+    }
+
+    private void Update()
+    {
+        if (currentBlock == null) return;
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && lastKeyPressed != KeyCode.LeftArrow)
+        {
+            currentBlock.transform.position = CreateRandomVectorLeft();
+            lastKeyPressed = KeyCode.LeftArrow;
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow) && lastKeyPressed != KeyCode.RightArrow)
+        {
+            currentBlock.transform.position = CreateRandomVectorRight();
+            lastKeyPressed = KeyCode.RightArrow;
+        }
+    }
+
+    private Vector3 CreateRandomVectorLeft() => CreateRandomVector(-6.5f, -1.5f);
+    private Vector3 CreateRandomVectorRight() => CreateRandomVector(1.5f, 6.5f);
+
+    private Vector3 CreateRandomVector(float minX, float maxX)
+    {
+        float randomX = Random.Range(minX, maxX);
+        return new Vector3(randomX, positionY, positionZ);
+    }
+}
