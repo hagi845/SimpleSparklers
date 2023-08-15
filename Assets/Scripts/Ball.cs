2512c46019d1743178a3b4dcc55a9083c8bd113f
hagi845 <gya132811@gmail.com>
2023-08-15 16:50:15 +0900

    壁を両端に追加しボールとの衝突判定を処理を追加する

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -4,11 +4,11 @@ public class Ball : MonoBehaviour
 {
     public float Speed = 5;
 
-    private Rigidbody2D rb;
+    private Rigidbody rb;
 
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody>();
         rb.velocity = new Vector2(Speed,  0);
     }
 
@@ -22,5 +22,11 @@ public class Ball : MonoBehaviour
         {
             rb.velocity = new Vector2(Speed, rb.velocity.y);
         }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // TODO: 
+        Debug.Log("gameover???");
     }
 }
