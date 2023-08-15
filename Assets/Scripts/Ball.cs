8a3f4caea14075a82140c613140e148ba2e28bc3
hagi845 <gya132811@gmail.com>
2023-08-15 12:17:59 +0900

    左右キーでボールの進行方向を変更できるようにする

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,18 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
     public float Speed = 5;
 
+    private Rigidbody2D rb;
+
     void Start()
     {
-        GetComponent<Rigidbody>().AddForce(Speed, 0, 0, ForceMode.VelocityChange);
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(Speed,  0);
     }
 
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.velocity = new Vector2(-Speed, rb.velocity.y);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.velocity = new Vector2(Speed, rb.velocity.y);
+        }
     }
 }
