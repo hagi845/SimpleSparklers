f888c92141f64822fcae8795f852801172b38be5
hagi845 <gya132811@gmail.com>
2023-08-17 22:53:13 +0900

    メニュー画面のBGMを追加

--- Assets/Scripts/GameStartControl.cs
+++ Assets/Scripts/GameStartControl.cs
@@ -5,6 +5,15 @@ using UnityEngine.SceneManagement;
 
 public class GameStartControl : MonoBehaviour
 {
+    private AudioManager bgm;
+
+    private void Start()
+    {
+        var background = GameObject.Find("BGM");
+        if (background != null) bgm = background.GetComponent<AudioSource>().GetComponent<AudioManager>();
+        bgm.Play();
+    }
+
     public void StartGame()
     {
         SceneManager.LoadScene("SampleScene");
